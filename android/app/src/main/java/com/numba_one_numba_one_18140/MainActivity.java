b2f45e4dc5591e1b56cf2273a0f4bd85deafbc22
Crowdbotics <team@crowdbotics.com>
2020-06-16 17:53:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/numba_one_numba_one_18140/MainActivity.java
@@ -0,0 +1,15 @@
+package com.numba_one_numba_one_18140;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "numba_one_numba_one_18140";
+    }
+}
